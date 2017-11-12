6a7c61628e6dfe02cd47c38cb1f2a2c5455492a4
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2017-11-12 15:04:31 +0100

    testing geofire API

--- de.cdv.mauar.backend.rest/src/main/java/de/cdv/mauar/backend/rest/service/FirebaseService.java
+++ de.cdv.mauar.backend.rest/src/main/java/de/cdv/mauar/backend/rest/service/FirebaseService.java
@@ -2,14 +2,18 @@ package de.cdv.mauar.backend.rest.service;
 
 import java.io.FileInputStream;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoFire.CompletionListener;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
+import com.firebase.geofire.LocationCallback;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 // https://firebase.google.com/docs/database/admin/retrieve-data
 public class FirebaseService {
@@ -20,27 +24,87 @@ public class FirebaseService {
 
 		FirebaseOptions options = new FirebaseOptions.Builder()
 		  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-		  .setDatabaseUrl("https://mauar-cc91a.firebaseio.com/")
-		  .setStorageBucket("photos")
+		  .setDatabaseUrl("https://mauar-cc91a.firebaseio.com")
+		  //.setStorageBucket("photos")
 		  .build();
 
 		FirebaseApp photosApp = FirebaseApp.initializeApp(options, "photos");
-		System.out.println(photosApp.getName());
 		
-		DatabaseReference databaseReference = FirebaseDatabase.getInstance(photosApp).getReference();
+		DatabaseReference ref = FirebaseDatabase.getInstance(photosApp).getReference();
+		GeoFire geoFire = new GeoFire(ref);
 		
-		ValueEventListener listener = new ValueEventListener() {
+		geoFire.setLocation("F-015005", new GeoLocation(37.7853889, -122.4056973), new CompletionListener() {
 		    @Override
-		    public void onDataChange(DataSnapshot dataSnapshot) {
-		    	PhotoLocation photoLocation = dataSnapshot.getValue(PhotoLocation.class);
-		        System.out.println(dataSnapshot.getKey() + " is located " + photoLocation.getUrl() + " here.");
+		    public void onComplete(String key, DatabaseError error) {
+		        if (error != null) {
+		            System.err.println("There was an error saving the location to GeoFire: " + error);
+		        } else {
+		            System.out.println("Location saved on server successfully!");
+		        }
+		    }
+		});
+		
+		geoFire.getLocation("F-015005", new LocationCallback() {
+		    @Override
+		    public void onLocationResult(String key, GeoLocation location) {
+		        if (location != null) {
+		            System.out.println(String.format("The location for key %s is [%f,%f]", key, location.latitude, location.longitude));
+		        } else {
+		            System.out.println(String.format("There is no location for key %s in GeoFire", key));
+		        }
 		    }
 
 		    @Override
 		    public void onCancelled(DatabaseError databaseError) {
+		        System.err.println("There was an error getting the GeoFire location: " + databaseError);
 		    }
-		};
-		databaseReference.addValueEventListener(listener);
-		//databaseReference.addListenerForSingleValueEvent(listener);
+		});		
+		
+		GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(37.7832, -122.4056), 0.6);
+		
+		geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+		    @Override
+		    public void onKeyEntered(String key, GeoLocation location) {
+		        System.out.println(String.format("Key %s entered the search area at [%f,%f]", key, location.latitude, location.longitude));
+		    }
+
+		    @Override
+		    public void onKeyExited(String key) {
+		        System.out.println(String.format("Key %s is no longer in the search area", key));
+		    }
+
+		    @Override
+		    public void onKeyMoved(String key, GeoLocation location) {
+		        System.out.println(String.format("Key %s moved within the search area to [%f,%f]", key, location.latitude, location.longitude));
+		    }
+
+		    @Override
+		    public void onGeoQueryReady() {
+		        System.out.println("All initial data has been loaded and events have been fired!");
+		    }
+
+		    @Override
+		    public void onGeoQueryError(DatabaseError error) {
+		        System.err.println("There was an error with this query: " + error);
+		    }
+		});		
+		
+		geoQuery.setCenter(new GeoLocation(37.7833, -122.4056));
+		
+//		DatabaseReference databaseReference = FirebaseDatabase.getInstance(photosApp).getReference();
+//		
+//		ValueEventListener listener = new ValueEventListener() {
+//		    @Override
+//		    public void onDataChange(DataSnapshot dataSnapshot) {
+//		    	PhotoLocation photoLocation = dataSnapshot.getValue(PhotoLocation.class);
+//		        System.out.println(dataSnapshot.getKey() + " is located " + photoLocation.getUrl() + " here.");
+//		    }
+//
+//		    @Override
+//		    public void onCancelled(DatabaseError databaseError) {
+//		    }
+//		};
+//		databaseReference.addValueEventListener(listener);
+//		//databaseReference.addListenerForSingleValueEvent(listener);
 	}
 }
